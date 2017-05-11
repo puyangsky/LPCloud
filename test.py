542182805b5d693fd7d32de0593f79ad484aece1
puyangsky <puyangsky@pku.edu.cn>
2017-05-11 17:45:54 +0800

    Added backend  module

--- test.py
+++ test.py
@@ -1,9 +1,25 @@
-def show():
-	name = "pyt"
-	age = 18
-	def inner(a):
-		print "name:", name, "\nage", age, "\nparam:", a
-	return inner
+from oslo_log import log as logging
+#print __name__
+LOG = logging.getLogger("test")
 
-method = show()
-method("test")
+def parse_diff():
+	f1 = open("./func_name.txt", "r")
+	#print f1.read()
+	names = f1.read().split("\n")
+	#print names
+	f2 = open("./func_res.txt", "r")
+	names1 = f2.read().split("\n")
+	#print names1
+	index = 0
+	for i in names:
+		if i != "" and i not in names1:
+			print i
+			index += 1
+	print("\n\rtotal diff: %d\n" % index)
+
+
+def test():
+	LOG.debug("fucking log")
+
+test()
+LOG.info("Python Standard Logging")
--- test2.py
+++ test2.py
@@ -1,17 +1,18 @@
 import sys
 import os
-def detailtrace(info):
+def detailtrace():
   retStr = ""
   curindex=0
   f = sys._getframe()
   f = f.f_back    # first frame is detailtrace, ignore it
   while hasattr(f, "f_code"):
     co = f.f_code
-    retStr = "%s(%s:%s)->"%(os.path.basename(co.co_filename),
+    retStr = "%s(%s:%s)\n"%(os.path.abspath(co.co_filename),
          co.co_name,
          f.f_lineno) + retStr
     f = f.f_back
-  print retStr+info
+  with open("/opt/stack/stack.log", "a+") as f1:
+	f1.write("%s\n" % retStr)
 
 
 

ef51fecb520479402e394d2199affb6ebb420520
puyangsky <puyangsky@pku.edu.cn>
2017-07-29 02:16:20 +0800

    还是无法处理中文乱码问题，但是已经找到问题出在读文件时乱码

---
+++ src/main/java/com/springboot/interceptor/ApplicationEventListener.java
@@ -0,0 +1,10 @@
+package com.springboot.interceptor;
+
+/**
+ * Author:      puyangsky
+ * Date:        17/7/29 上午2:09
+ * Method:
+ * Difficulty:
+ */
+public class ApplicationEventListener {
+}
---
+++ src/main/java/com/springboot/service/RoleService.java
@@ -0,0 +1,10 @@
+package com.springboot.service;
+
+/**
+ * Author:      puyangsky
+ * Date:        17/7/29 上午1:25
+ * Method:
+ * Difficulty:
+ */
+public class RoleService {
+}
